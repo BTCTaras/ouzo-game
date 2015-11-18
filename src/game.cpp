292d2324d35a708ee6a25cfcd3650194e01c6d13
GeorgeMcKinlay <grgmckinlay@gmail.com>
2015-11-18 18:11:58 +0100

    Switched to a pure core GL context

--- src/game.cpp
+++ src/game.cpp
@@ -21,6 +21,11 @@ CGame::CGame()
 	:	m_targetFPS(60),
 		m_running(true)
 {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Forbid compatibility
+
 	m_window = glfwCreateWindow(GAME_START_WIDTH, GAME_START_HEIGHT, GAME_START_TITLE, NULL, NULL);
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
