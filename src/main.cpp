495bcc21914f9ea5bdf6abc0c16ab10db2f832a0
GeorgeMcKinlay <grgmckinlay@gmail.com>
2015-11-25 00:11:13 +0100

    Removed WinMain in favour of SDL_main

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #include <GL/glew.h>
 
+#include <SDL2/SDL_main.h>
+
 int main(int argc, char *argv[]) {
 	CGame game;
 	CGame::Inst = &game;
@@ -13,13 +15,3 @@ int main(int argc, char *argv[]) {
 	game.StartLoop();
 	return 0;
 }
-
-#ifdef _WIN32
-#include <Windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	char *args[1] = { "no" };
-	return main(0, args); // no arguments for windows because it's a cunt
-}
-
-#endif
