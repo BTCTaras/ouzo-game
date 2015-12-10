d4d4575a16317ca6b7f741041c3ffb5533b47206
GeorgeMcKinlay <grgmckinlay@gmail.com>
2015-12-10 12:57:02 +0100

    Added OpenAL to CMakeLists

--- src/graphics_common/sprite_verts.cpp
+++ src/graphics_common/sprite_verts.cpp
@@ -2,10 +2,10 @@
 #include "graphics_common/graphics.hpp"
 
 const vertex_t SPRITE_VERTS[] = {
-	0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f,
-	1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-	1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+	{ 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f },
+	{ 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f },
+	{ 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f },
+	{ 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f },
 };
 
 const size_t SPRITE_VERTS_SIZE = sizeof(SPRITE_VERTS);
